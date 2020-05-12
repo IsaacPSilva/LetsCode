50112a0ff2bcf9bb1a73f148a4cc6821e2bd2c87
IsaacPSilva <isaacsilva.ti@outlook.com>
2020-05-11 23:28:55 -0300

    Primeiras atividades

---
+++ Aula 01/[Exercicio 01] Hello World.py
@@ -0,0 +1,4 @@
+#1. Faça um programa que mostre a mensagem "Olá, mundo!" na tela.
+
+hello = 'Hello World'
+print (hello)
---
+++ Aula 01/[Exercicio 02] Informando número.py
@@ -0,0 +1,7 @@
+#2. Faça um programa que peça um número e mostre a mensagem "O número informado foi [número]".
+
+#Informando número
+num = int(input('Digite um número: '))
+
+#Imprimindo número
+print (f'O numero informado foi {num}')
