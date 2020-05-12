36a2678234522ec5d5dcd9f9c4bc60f0d4014ba8
IsaacPSilva <isaacsilva.ti@outlook.com>
2020-05-11 23:59:25 -0300

    Create [Exercicio 10] Data completa.py

---
+++ Aula 01/[Exercicio 10] Data completa.py
@@ -0,0 +1,13 @@
+#10. Faça um programa que peça o dia, o mês e o ano para o usuário e imprima a data completa no formato dd/mm/aaaa.
+
+
+print('IMPRIMINDO DATA INFORMADA')
+print('-'*40)
+
+#Informando valores
+dia = input('Informe o dia: ')
+mes = input('Informe o mês: ')
+ano = input('Informe o ano: ')
+
+#Imprimindo data
+print(f'Data informada: {dia}/ {mes}/ {ano}')
