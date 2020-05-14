291ffb2a1a8ad4ba3f710a9c761fe20b602f5f46
IsaacPSilva <isaacsilva.ti@outlook.com>
2020-05-13 23:54:55 -0300

    Create [Exercicio 02] IF - Checking Number, Positivo or Negative.py

---
+++ Aula 02/[Exercicio 02] IF - Checking Number, Positivo or Negative.py
@@ -0,0 +1,14 @@
+'''2. Faça um programa que peça um número e mostre se ele é positivo
+ou negativo.'''
+
+print('TESTING NUMBERS. POSITIVE OR NEGATIVE')
+print('-'*30)
+
+#Receiving number
+num = input('ENTER THE NUMBER: ')
+
+#Checking number
+if num >=0:
+    print('The number is positive.')
+else:
+    print('The number is negative.')
