f33459a9da5e53921af9dc2648027bb8fc2f40fd
IsaacPSilva <isaacsilva.ti@outlook.com>
2020-05-14 00:11:43 -0300

    Information update

---
+++ Aula 02/[Exercicio 04] Testing larger number.py
@@ -0,0 +1,15 @@
+'''4. Faça um programa que peça dois números e mostre o maior deles.'''
+
+print('Checking number')
+
+#Receiving number
+num1 = int(input('Insert the fist number: '))
+num2 = int(input('Insert the second number: '))
+
+#Checking information and printing
+if num1>num2:
+    print(f'The number {num1} is greater.')
+elif num2>num:
+    print(f'The number {num2} is greater.')
+else:
+    print('The numbers is equals.')
