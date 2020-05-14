858bcb947eb0c0e267d21e0d7ef2f08dd2b82791
IsaacPSilva <isaacsilva.ti@outlook.com>
2020-05-14 00:00:01 -0300

    Create [Exercicio 04] Maior ou menos numero.py

---
+++ Aula 02/[Exercicio 04] Maior ou menos numero.py
@@ -0,0 +1,8 @@
+'''4. Faça um programa que peça dois números e mostre o maior deles.'''
+
+num = int(input('Informe o primeiro numero: '))
+num2 = int(input('Informe o segundo numero: '))
+if num>num2:
+    print(f'O número {num} é maior.')
+elif num2>num:
+    print(f'O numero {num2} é maior')
