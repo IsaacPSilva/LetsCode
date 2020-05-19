aac1f1930edda35cb20b78533978023a3f930f6e
IsaacPSilva <isaacsilva.ti@outlook.com>
2020-05-18 23:16:09 -0300

    Create Coding.py

---
+++ Aula 04/Coding.py
@@ -0,0 +1,38 @@
+
+
+
+'''animais = ['boi', 'cavalo', 'bode']
+'''
+
+
+#Adicionando elementos em uma lista dentro de uma WHILE
+'''res = input('Você quer acrescentar um animal a lista ? ')
+
+while res=='s' or res=='sim':
+    animal = input('Digite um animal novo: ')
+    animais.append(animal)
+    res = input('Você quer acrescentar um animal a lista ? ')'''
+
+
+#Remove elemento dentro da lista
+'''animais.remove('cavalo')
+print(animais)'''
+
+
+#Ordenar Lista
+'''animais.sort
+print(animais)'''
+
+#Verificar se tem cavalo dentro da lista animais
+'''if 'cavalo' in animais:
+    print('tem cavalo')'''
+
+#Percorrer uma lista
+'''for animal in animais:
+    print(animal)'''
+
+
+
+lista = [1,2,3,4,5,6,7,8,9]
+lista2 = []
+
