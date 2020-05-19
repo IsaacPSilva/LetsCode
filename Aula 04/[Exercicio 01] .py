dee8f7f4b422d03c53d0ab5f471b04b5027846af
IsaacPSilva <isaacsilva.ti@outlook.com>
2020-05-18 23:14:46 -0300

    Create [Exercicio 01] .py

---
+++ Aula 04/[Exercicio 01] .py
@@ -0,0 +1,11 @@
+'''1. Crie uma lista qualquer e faça um programa que imprima cada
+elemento da lista usando o for.'''
+
+animais = ['Boi', 'Cavalo', 'Cachorro', 'Gato', 'Pagagaio']
+
+for i in animais:
+    print(i)
+
+'''    2. Faça um programa que imprima todos os itens de uma lista usando
+while e compare com o exercício 1.'''
+
