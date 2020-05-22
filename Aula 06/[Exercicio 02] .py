41d64efadd2e4cfabe4dd07dcd810eb05f6692b1
IsaacPSilva <isaacsilva.ti@outlook.com>
2020-05-21 23:48:52 -0300

    Create [Exercicio 02] .py

---
+++ Aula 06/[Exercicio 02] .py
@@ -0,0 +1,9 @@
+'''2. Faça uma função que recebe o valor do raio de um círculo e retorna o
+valor do comprimento de sua circunferência: C = 2*pi*r.'''
+
+#Definir função
+def raio(num):
+    c = 2*3.14*num
+    print(f'O valor do comprimento é {c:.2f}')
+
+raio(20)
