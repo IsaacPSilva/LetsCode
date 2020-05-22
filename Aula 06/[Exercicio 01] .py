5a9b4e6379926a9d3b1c0c93baf16f87d68fc356
IsaacPSilva <isaacsilva.ti@outlook.com>
2020-05-21 23:48:43 -0300

    Create [Exercicio 01] .py

---
+++ Aula 06/[Exercicio 01] .py
@@ -0,0 +1,8 @@
+'''1. Faça uma função que recebe um número e imprime seu dobro.'''
+
+#Definir a função
+def dobro(num):
+    tot = num * 2
+    print(f'O valor do dobro de {num} é {tot}')
+
+dobro(10)
